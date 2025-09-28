9e8835bd0664e5ade48a761a5efc0af7c0521b60
100lyf <100lyf100@gmail.com>
2025-09-28 18:36:37 -0500

    Fix GLB path for GitHub Pages

--- src/components/IPhone.jsx
+++ src/components/IPhone.jsx
@@ -11,7 +11,7 @@ import React, { useEffect, useRef } from 'react'
 import { useGLTF, useTexture } from '@react-three/drei'
 
 export function Model(props) {
-  const { nodes, materials } = useGLTF('/models/scene.glb');
+  const { nodes, materials } = useGLTF(`${import.meta.env.BASE_URL}models/scene.glb`);
 
 
 
@@ -260,4 +260,4 @@ export function Model(props) {
 
 export default Model;
 
-useGLTF.preload("/models/scene.glb");
+useGLTF.preload(`${import.meta.env.BASE_URL}models/scene.glb`);
