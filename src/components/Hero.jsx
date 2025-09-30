dd66732f3dd23af32823b5efa9bc1f6da5f95bbf
100lyf <100lyf100@gmail.com>
2025-09-30 11:30:47 -0500

    removed poster img from desktop screen

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,65 +1,74 @@
 import gsap from 'gsap';
-import {useGSAP} from '@gsap/react';
-import {heroVideo, smallImg, smallHeroVideo} from '../utils';
-import { useState, useEffect} from 'react';
+import { useGSAP } from '@gsap/react';
+import { heroVideo, smallImg, smallHeroVideo } from '../utils';
+import { useState, useEffect } from 'react';
 
 const Hero = () => {
-  const [videoSrc, setVideoSrc] = useState(window.
-    innerWidth < 760 ? smallHeroVideo : heroVideo)
-  
-    const handleVideoSrcSet = () => {
-      if(window.innerWidth < 760) {
-        setVideoSrc(smallHeroVideo)
-      } else{
-        setVideoSrc(heroVideo)
-      }
+  const [videoSrc, setVideoSrc] = useState(
+    window.innerWidth < 760 ? smallHeroVideo : heroVideo
+  );
+  const [posterSrc, setPosterSrc] = useState(
+    window.innerWidth < 760 ? smallImg : undefined
+  );
+
+  const handleVideoSrcSet = () => {
+    if (window.innerWidth < 760) {
+      setVideoSrc(smallHeroVideo);
+      setPosterSrc(smallImg);
+    } else {
+      setVideoSrc(heroVideo);
+      setPosterSrc(undefined);
     }
-  
-   useEffect(() => {
-   window.addEventListener('resize', handleVideoSrcSet);
-   return () => {
-    window.removeEventListener('resize',
-      handleVideoSrcSet)
-   }
-   }, [])
+  };
 
-    useGSAP(() => {
-    gsap.to('#hero', {opacity: 1, delay: 2})
-    gsap.to('#cta', {opacity: 1, y: -50, delay: 2})
-  }, [])
+  useEffect(() => {
+    window.addEventListener('resize', handleVideoSrcSet);
+    return () => {
+      window.removeEventListener('resize', handleVideoSrcSet);
+    };
+  }, []);
+
+  useGSAP(() => {
+    gsap.to('#hero', { opacity: 1, delay: 2 });
+    gsap.to('#cta', { opacity: 1, y: -50, delay: 2 });
+  }, []);
+
   return (
-    <section className="w-full nav-height
-    bg-black relative">
-     <div className="h-5/6 w-full 
-     flex-center flex-col">
-      <p id="hero" className="hero-title">iPhone 15 Pro</p>
-     
-     <div className= "md:w-10/12 w-9/12">
-      <video className= "pointer-events-none" 
-      autoPlay 
-      muted 
-      playsInline={true}
-      loop
-      controls={false} 
-      poster= {smallImg}
+    <section className="w-full nav-height bg-black relative">
+      <div className="h-5/6 w-full flex-center flex-col">
+        <p id="hero" className="hero-title">
+          iPhone 15 Pro
+        </p>
 
-      key= {videoSrc}>
-        <source src={videoSrc} type="video/mp4" />
-        </video>
-     </div>
-     </div>
+        <div className="md:w-10/12 w-9/12">
+          <video
+            className="pointer-events-none"
+            autoPlay
+            muted
+            playsInline
+            loop
+            controls={false}
+            poster={posterSrc}
+            key={videoSrc}
+          >
+            <source src={videoSrc} type="video/mp4" />
+          </video>
+        </div>
+      </div>
 
-<div
-  id="cta"
-  className="flex flex-col items-center
-   opacity-0 translate-y-20">
-    <a href="#highlights" className="btn">Buy</a>
-     <p className="font-normal text-xl">From $199/month
-       or $999</p>
-</div>
-
+      <div
+        id="cta"
+        className="flex flex-col items-center opacity-0 translate-y-20"
+      >
+        <a href="#highlights" className="btn">
+          Buy
+        </a>
+        <p className="font-normal text-xl">
+          From $199/month or $999
+        </p>
+      </div>
     </section>
-  )
-}
+  );
+};
 
-export default Hero
+export default Hero;
